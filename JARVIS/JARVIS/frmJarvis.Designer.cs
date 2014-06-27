e4a1b4778679b9f9246fe1df5ac6bc7b9ea4db99
Nam Tran <tranngocnam97@yahoo.com>
2014-06-27 13:52:43 -0400

    Implemented Speech API
    - Added working speech narration (uses Microsft Anna voice for now)
    - Added simple speech recognition (needs tuning and adjustment)
    - Made working output method for printing to the output field

--- JARVIS/JARVIS/frmJarvis.Designer.cs
+++ JARVIS/JARVIS/frmJarvis.Designer.cs
@@ -31,6 +31,7 @@
             this.btnSettings = new System.Windows.Forms.Button();
             this.txtInput = new System.Windows.Forms.TextBox();
             this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnInput = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSettings
@@ -45,15 +46,17 @@
             // txtInput
             // 
             this.txtInput.BackColor = System.Drawing.Color.MediumBlue;
+            this.txtInput.ForeColor = System.Drawing.Color.White;
             this.txtInput.Location = new System.Drawing.Point(12, 480);
             this.txtInput.Name = "txtInput";
-            this.txtInput.Size = new System.Drawing.Size(577, 20);
+            this.txtInput.Size = new System.Drawing.Size(496, 20);
             this.txtInput.TabIndex = 1;
-            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
             // 
             // txtOutput
             // 
             this.txtOutput.BackColor = System.Drawing.Color.MediumBlue;
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.ForeColor = System.Drawing.Color.White;
             this.txtOutput.Location = new System.Drawing.Point(12, 12);
             this.txtOutput.Multiline = true;
             this.txtOutput.Name = "txtOutput";
@@ -61,17 +64,30 @@
             this.txtOutput.Size = new System.Drawing.Size(577, 462);
             this.txtOutput.TabIndex = 2;
             // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(514, 478);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(75, 23);
+            this.btnInput.TabIndex = 3;
+            this.btnInput.Text = "Read";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
             // frmJarvis
             // 
+            this.AcceptButton = this.btnInput;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkBlue;
             this.ClientSize = new System.Drawing.Size(659, 512);
+            this.Controls.Add(this.btnInput);
             this.Controls.Add(this.txtOutput);
             this.Controls.Add(this.txtInput);
             this.Controls.Add(this.btnSettings);
             this.Name = "frmJarvis";
             this.Text = "JARVIS";
+            this.Load += new System.EventHandler(this.frmJarvis_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,6 +98,7 @@
         private System.Windows.Forms.Button btnSettings;
         private System.Windows.Forms.TextBox txtInput;
         private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnInput;
     }
 }
 
--- JARVIS/JARVIS/frmJarvis.cs
+++ JARVIS/JARVIS/frmJarvis.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,14 +14,70 @@ namespace JARVIS
 {
     public partial class frmJarvis : Form
     {
+        // Speech recognition engine w/ US English as the langauge
+        private SpeechRecognitionEngine recognition = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
+        // Text to Speech
+        private SpeechSynthesizer speech = new SpeechSynthesizer();
+
         public frmJarvis()
         {
+            // Loads the form components
             InitializeComponent();
         }
 
-        private void txtInput_TextChanged(object sender, EventArgs e)
+        // Writes a message to the text output field with a date/time stamp and the message 
+        public void writeToOutput(String output)
         {
+            // Adds the new output message to a new line in the output text field
+            txtOutput.AppendText(System.Environment.NewLine + "[" + System.DateTime.Now + "] " + output);
+        }
 
+        // Makes JARVIS say something with text to speech as well as prints it to the console
+        public void say(String message)
+        {
+            // Reads the message as speech
+            speech.Speak(message);
+            // Writes the message to the output text field
+            writeToOutput("JARVIS: " + message);
+        }
+
+        // Event handler for when speech is recognised
+        public void recognition_SpeechRecognised(object sender, SpeechRecognizedEventArgs e)
+        {
+            // Writes the recognised text to the input text field and outputs it to the output text field
+            txtInput.Text = e.Result.Text;
+            writeToOutput("USER: " + txtInput.Text);
+            txtInput.Clear();
+        }
+
+        // Runs when the form is loaded
+        private void frmJarvis_Load(object sender, EventArgs e)
+        {
+            // Creates a grammar to understand most English sentences
+            DictationGrammar grammar = new DictationGrammar();
+            grammar.Name = "Default Grammar";
+            grammar.Enabled = true;
+
+            // Loads the grammar to the speech recognition engine
+            recognition.LoadGrammarAsync(grammar);
+
+            // Sets the recognition engine to the computer's default audio input device and starts recognising speech
+            recognition.SetInputToDefaultAudioDevice();
+            // Adds an event handler for when the speech recognition understands something was said
+            recognition.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognition_SpeechRecognised);
+            // Begins a recognition thread
+            recognition.RecognizeAsync(RecognizeMode.Multiple);
+
+            // Message to confirm that JARVIS has been loaded
+            say("I have been fully loaded, sir.");
+        }
+
+        // Runs when the "Read" button is clicked or the ENTER key is pressed
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            // Outputs the user input to the output text field
+            writeToOutput("USER: " + txtInput.Text);
+            txtInput.Clear();
         }
     }
 }
