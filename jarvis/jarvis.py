099a33fc97e1240225c60b5369d18acb08cdc2a2
omn0mn0m <tranngocnam97@yahoo.com>
2017-04-08 01:52:27 -0400

    Added chatbot

--- jarvis/jarvis.py
+++ jarvis/jarvis.py
@@ -1,12 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 from tts import TTS
 
 tts = TTS()
 
+logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter'
+]
+
+preprocessors=[
+    'chatterbot.preprocessors.clean_whitespace'
+]
+
+#chatbot = ChatBot('JARVIS',
+#                  storage_adapter='chatterbot.storage.MongoDatabaseAdapter')
+chatbot = ChatBot('JARVIS')
+
+chatbot.set_trainer(ChatterBotCorpusTrainer)
+chatbot.train("chatterbot.corpus.english.greetings")
+
 input = ''
 
-tts.say('I have been fully loaded')
+tts.say("I have been fully loaded")
 
 while (input != 'quit'):
-    input = raw_input('User: ')
-    tts.say(input)
+    try:
+        input = raw_input('User: ')
+        tts.say(chatbot.get_response(input))
+    except:
+        break
 
