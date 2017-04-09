dfe3f708ce7ebce839beee33d9580a9225e94c8f
Nam Tran <tranngocnam97@yahoo.com>
2017-04-09 02:54:32 -0400

    Added speech input

--- jarvis/jarvis.py
+++ jarvis/jarvis.py
@@ -4,6 +4,8 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from tts import TTS
 
+import speech_recognition as sr
+
 tts = TTS()
 
 logic_adapters=[
@@ -22,14 +24,38 @@ chatbot = ChatBot('JARVIS')
 chatbot.set_trainer(ChatterBotCorpusTrainer)
 chatbot.train("chatterbot.corpus.english.greetings")
 
+recogniser = sr.Recognizer()
+mic = sr.Microphone()
+
+with mic  as source:
+    print "A moment of silence please..."
+    recogniser.adjust_for_ambient_noise(source)
+    
 input = ''
 
 tts.say("I have been fully loaded")
 
+use_speech = True
+
 while (input != 'quit'):
     try:
-        input = raw_input('User: ')
-        tts.say(chatbot.get_response(input))
+        if (use_speech):
+            print "Say something!"
+
+            with mic as source:
+                audio = recogniser.listen(source)
+
+            try:
+                input = recogniser.recognize_google(audio)
+                print 'User: {}'.format(input)
+                tts.say(chatbot.get_response(input))
+            except sr.UnknownValueError:
+                print "I didn't quite get that..."
+            except sr.RequestError as e:
+                print "Google Speech isn't working..."
+        else:
+            input = raw_input('User: ')
+            tts.say(chatbot.get_response(input))
     except:
         break
 
