2ebba6119c71fcac164ad03a8df83c6bc680473b
Nam Tran <tranngocnam97@yahoo.com>
2017-04-26 02:33:49 -0400

    Single user JARVIS chat (group chat too unstable)

--- jarvis/jarvis.py
+++ jarvis/jarvis.py
@@ -1,40 +1,79 @@
 import nltk
 import filemanager
+import multiprocessing
+import os
+import ConfigParser
 
-from assistant import Assistant
+from assistant import Assistant, Messenger
 
-use_speech = False
-nlp_debug = True
+resources_dir = 'resources\\'
 
-jarvis = Assistant(use_speech)
+login_creds = ConfigParser.SafeConfigParser()
 
-jarvis.say('I have been fully loaded')
+if os.path.isfile(resources_dir + 'login_creds.cfg'):
+    login_creds.read(resources_dir + 'login_creds.cfg')
+else:
+    print "No logins... creating now"
+    new_login_creds = open(resources_dir + 'login_creds.cfg', 'w')
+    login_creds.write(new_login_creds)
+    new_login_creds.close()
+    
 
-input = ''
+def fb_worker(email, password):
+    messenger = Messenger(email, password)
+    messenger.listen()
+    return
 
-while (input != 'quit'):
-    try:
-        input = jarvis.get_input()
+if __name__ == '__main__':
+    use_speech = False
+    nlp_debug = True
 
-        if not input == '':
-            words = nltk.word_tokenize(input)
-            tagged = nltk.pos_tag(words)
+    jarvis = Assistant(use_speech)
+    
+    jarvis.say('I have been fully loaded')
+    
+    input = ''
+
+    while (input != 'Goodbye JARVIS'):
+        try:
+            input = jarvis.get_input()
             
-            verbs = []
-            
-            for word in tagged:
-                if 'VB' in word[1]:
-                    verbs.append(word[0].lower())
+            if not input == '':
+                words = nltk.word_tokenize(input)
+                tagged = nltk.pos_tag(words)
+                
+                verbs = []
+                proper_nouns = []
+                
+                for word in tagged:
+                    if 'VB' in word[1]:
+                        verbs.append(word[0].lower())
+                    elif word[1] == 'NNP':
+                        proper_nouns.append(word[0].lower())
+                        
+                if nlp_debug:
+                    print 'Tags: {}'.format(tagged)
+                    print 'Verbs: {}'.format(verbs)
+                    
+                if "open" in verbs:
+                    jarvis.say(filemanager.try_open_executable(words, tagged))
+                elif "respond" in verbs:
+                    if "facebook" in proper_nouns:
+                        if not login_creds.has_section('Facebook'):
+                            login_creds.add_section('Facebook')
+                            login_creds.set('Facebook', 'email', raw_input('Enter your FB email: '))
+                            login_creds.set('Facebook', 'password', raw_input('Enter your FB password: '))
 
-            if nlp_debug:
-                print 'Tags: {}'.format(tagged)
-                print 'Verbs: {}'.format(verbs)
+                        with open(resources_dir + 'login_creds.cfg', 'wb') as configfile:
+                            login_creds.write(configfile)
 
-            if "open" in verbs:
-                jarvis.say(filemanager.try_open_executable(words, tagged))
-            else:
-                jarvis.respond(input)
-    except Exception as e:
-        print e
-        break
-
+                        fb_process = multiprocessing.Process(target = fb_worker, args = (login_creds.get('Facebook', 'email'), login_creds.get('Facebook', 'password')))
+                        fb_process.daemon = True
+                        fb_process.start()
+                else:
+                    jarvis.respond(input)
+        except Exception as e:
+            print e
+            fb_process.terminate()
+            fb_process.join()
+            break
